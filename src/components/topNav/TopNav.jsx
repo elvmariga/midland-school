8a456b7a41f275902b0af785ab11bf47defcb1c6
elvmariga <elvmariga@gmail.com>
2022-10-15 06:52:39 +0300

    Added css for top nav bar with links

--- src/components/topNav/TopNav.jsx
+++ src/components/topNav/TopNav.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Socials } from "../socials/Socials";
+import "./style/style.css"
 
 export const TopNav = () => {
   return (
@@ -10,17 +11,38 @@ export const TopNav = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <i class="fa-regular fa-envelope"></i> info@midland.co.ke
+          <i class="fa-regular fa-envelope fa-xl"></i> info@midland.co.ke
         </a>
       </div>
 
       <div className="tel">
         {" "}
         <a href="tel:+2541234555">
-          <i class="fa-solid fa-phone"></i> +2541234555
+          <i class="fa-solid fa-phone fa-xl"></i> +2541234555
         </a>
       </div>
       <div className="socials">{Socials}</div>
+      <div className="links">
+        <a href="http:// ">
+          {" "}
+          <i class="fa-brands fa-whatsapp fa-xl"></i>
+        </a>
+        <a href="http://">
+          {" "}
+          <i class="fa-brands fa-twitter fa-xl"></i>
+        </a>
+        <a href="http://">
+          {" "}
+          <i class="fa-brands fa-facebook fa-xl"></i>
+        </a>
+        <a href="http://">
+          {" "}
+          <i class="fa-brands fa-instagram fa-xl"></i>
+        </a>
+        <a href="http://">
+          <i class="fa-brands fa-linkedin-in fa-xl"></i>
+        </a>
+      </div>
       <div className="webmail">
         <a href="http://">Staff Web Mail</a>
       </div>
