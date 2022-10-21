69da9b4d04c741dcd33bdbd49f001b33a4f382d9
elvmariga <elvmariga@gmail.com>
2022-10-21 16:32:18 +0300

    glance page

--- src/components/glance/Glance.jsx
+++ src/components/glance/Glance.jsx
@@ -3,37 +3,39 @@ import "./style/style.css"
 
 export const Glance = () => {
   return (
-    <div className="glance">
-      <div className="glanceTitle">
-        <h3>Midland Prep at a Glance</h3>
+    <div className="glance-container">
+      <div className="glance">
+        <div className="glanceTitle">
+          <h3>Midland Prep at a Glance</h3>
+        </div>
+        <div className="glanceStats">
+          <div>
+            <p className="statsNumbers">
+              <b>
+                <span id="value1">20</span>K+
+              </b>
+            </p>
+            <p className="statsTitle">Enrolled Students</p>
+          </div>
+          <div>
+            <p className="statsNumbers">
+              <b>
+                <span id="value2">20</span>K+
+              </b>
+            </p>
+            <p className="statsTitle">Enrolled Students</p>
+          </div>
+          <div>
+            <p className="statsNumbers">
+              <b>
+                <span id="values">20</span>K+
+              </b>
+            </p>
+            <p className="statsTitle">Enrolled Students</p>
+          </div>
+        </div>
+        {/* <hr /> */}
       </div>
-      <div className="glanceStats">
-        <div>
-          <p className="statsNumbers">
-            <b>
-              <span id="value1">20</span>K+
-            </b>
-          </p>
-          <p className="statsTitle">Enrolled Students</p>
-        </div>
-        <div>
-          <p className="statsNumbers">
-            <b>
-              <span id="value2">20</span>K+
-            </b>
-          </p>
-          <p className="statsTitle">Enrolled Students</p>
-        </div>
-        <div>
-          <p className="statsNumbers">
-            <b>
-              <span id="values">20</span>K+
-            </b>
-          </p>
-          <p className="statsTitle">Enrolled Students</p>
-        </div>
-      </div>
-      {/* <hr /> */}
     </div>
   );
   
