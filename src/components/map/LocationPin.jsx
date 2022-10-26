3a41a98eea960f14ee370248ec527001d4581811
boaz serem <boaserem022@gmail.com>
2022-10-26 11:34:10 +0300

    added google maps

--- src/components/map/LocationPin.jsx
+++ src/components/map/LocationPin.jsx
@@ -1,9 +1,10 @@
-import { ImLocation2} from "react-icons/im";
+import { Icon } from '@iconify/react'
+import locationIcon from '@iconify/icons-mdi/map-marker'
 import React from "react";
 
 export const LocationPin = ({ text }) => (
     <div className="pin">
-      <ImLocation2  className="pin-icon" />
+      <Icon icon={locationIcon} className="pin-icon" />
       <p className="pin-text">{text}</p>
     </div>
 )
