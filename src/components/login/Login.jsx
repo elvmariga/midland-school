eb8b9874165ed9d49c047e0a2d6fa0bcf832d2e8
boaz serem <boaserem022@gmail.com>
2022-11-01 16:53:35 +0300

    added regex funcionaly

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from "axios";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 // import Logo from "../../assests/logo.png"
 import "./style/style.css";
-import { ToastContainer, toast} from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { validEmail, validPassword } from "../regex/Regex";
 
 // toast.configure()
 
-export  const Login = () => {
+export const Login = () => {
 
-  const [formData, updateFormData] = React.useState();
-  const [isLoading, setIsLoading] = React.useState(false);
+  const [formData, updateFormData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [emailErr, setEmailErr] = useState(false);
+  const [pwdError, setPwdError] = useState(false);
+
+  const validate = () => {
+    if (!validEmail.test(formData.email)) {
+      setEmailErr(true);
+    }
+    if (!validPassword.test(formData.password)) {
+      setPwdError(true);
+    }
+  };
 
   const handleChange = (e) => {
+    
     updateFormData({
       ...formData,
 
@@ -21,30 +35,31 @@ export  const Login = () => {
       [e.target.name]: e.target.value.trim()
     });
   };
-
+  
   const handleSubmit = (e) => {
+   
     e.preventDefault()
-    
+    validate();
     // ... submit to API or something
     logInUser();
   };
 
   const logInUser = async () => {
-    
-    try{
+
+    try {
       setIsLoading(true);
 
       const res = await axios({
         method: "post",
         url: "https://ba57-102-68-77-133.ap.ngrok.io/api/auth/login",
         data: formData,
-       
+
       })
-     
+
       console.log(res);
-      if(res.data.status === "success" ){
+      if (res.data.status === "success") {
         notify();
-        window.localStorage.setItem("token",res.data.data);
+        window.localStorage.setItem("token", res.data.data);
         // window.location.href=`https://9568-102-68-77-133.ap.ngrok.io/admin?token=${res.data.token}`
         console.log(res);
       }
@@ -53,10 +68,10 @@ export  const Login = () => {
     catch (error) {
 
       console.log(error);
-      
+
     }
 
-    finally{
+    finally {
       setIsLoading(false);
     }
 
@@ -64,10 +79,10 @@ export  const Login = () => {
   }
 
   //diaplaying succes message after user login
-  const notify = () =>{
- 
+  const notify = () => {
+
     // Calling toast method by passing string
-    toast.success("Log in succesful, welcome", {autoClose:3000});
+    toast.success("Log in succesful, welcome", { autoClose: 3000 });
   }
 
   return isLoading ? (
@@ -81,6 +96,9 @@ export  const Login = () => {
         </div> */}
         <div className="signin-right">
           <form>
+
+            {emailErr && <p>Your email is invalid</p>}
+            {pwdError && <p>Your password is invalid</p>}
             <h2>Sign In</h2>
 
             <div className="mb-3">
@@ -125,7 +143,7 @@ export  const Login = () => {
               </div>
             </div>
 
-            
+
             <div className="d-grid">
               <button
                 onClick={handleSubmit}
@@ -134,11 +152,11 @@ export  const Login = () => {
                 className="btn btn-primary myBtn ">
                 Submit
               </button>
-              </div>
+            </div>
           </form>
         </div>
       </div>
     </div>
   );
-  
+
 };
