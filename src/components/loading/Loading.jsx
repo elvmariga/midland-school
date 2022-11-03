7814c58e7d9788731127047c2d491c966069aed4
elvmariga <elvmariga@gmail.com>
2022-11-03 11:52:09 +0300

    styled loading gif

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactLoading from "react-loading";
+import "./style/style.css"
 
 export const Loading = ({ type, color }) => (
-  <ReactLoading type={type} color={"#000000"} height={"5%"} width={"5%"} />
+  <div className="load">
+    <ReactLoading type={type} color={"#bd0204"} height={"5%"} width={"5%"} display={"flex"} justifyContent={"center"} alignItems={"center"} />
+  </div>
 );
 
