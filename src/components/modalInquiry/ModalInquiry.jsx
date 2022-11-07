5fa0960b4c0fb6723f5b79bc199428e65f740121
boaz serem <boaserem022@gmail.com>
2022-11-07 12:13:26 +0300

    added diable feature to contact us form

--- src/components/modalInquiry/ModalInquiry.jsx
+++ src/components/modalInquiry/ModalInquiry.jsx
@@ -2,13 +2,23 @@ import React from "react";
 import "./style/style.css";
 import { ImCross } from "react-icons/im";
 import axios from "axios";
+import { Loading } from "../loading/Loading";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const ModalInquiry = ({ getModalState }) => {
   const [showModal, setShowModal] = React.useState(false);
-  const [formData, updateFormData] = React.useState();
+  const [formData, updateFormData] = React.useState({
+    name:"",
+    email:"",
+    tel:"",
+    subject:"",
+    message:"",
+  });
   const [isLoading, setIsLoading] = React.useState(false);
 
   const handleChange = (e) => {
+
     updateFormData({
       ...formData,
 
@@ -19,119 +29,127 @@ export const ModalInquiry = ({ getModalState }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    
+    notify();
     // ... submit to API or something
     sendInquiry();
   };
-
+console.log(formData)
   const sendInquiry = async () => {
-    
-    try{
+
+    try {
       setIsLoading(true);
 
       const res = await axios({
         method: "post",
         url: "https://ba57-102-68-77-133.ap.ngrok.io/api/enquire",
         data: formData,
-       
+
       })
-     
+
       console.log(res);
-      
+
     }
 
     catch (error) {
 
       console.log(error);
-      
+
     }
 
-    finally{
+    finally {
       setIsLoading(false);
     }
-
-
   }
-  return (
-    // showModal===true &&
-    <div className="cover">
-      <div className="contact">
-        <div className="close">
-          <ImCross
-            onClick={() => {
-              setShowModal(false);
-              getModalState(showModal);
-            }}
-            style={{
-              opacity: "0.9",
-              with: "25px",
-              padding: "5px",
-              color: "black",
-              display: "flex",
-              justifyItems: "end",
-              height: "30px",
-              width: "30px",
-              alignSelf: "end",
-            }}
-          />
-        </div>
+  //notify the uset that the response has been recieved
+  const notify = () => {
 
-        <div>
-          <h2>Any questions? Send us an inquiry</h2>
-        </div>
-        <div>
-          <div></div>
-          <form action="" method="post">
-            <input
-              onChange={handleChange}
-              type="text"
-              name="name"
-              id="name"
-              placeholder="Full Name"
-              required="required"
+    // Calling toast method by passing string
+    toast.success("Thank you for your inquiry we will get in touch soon", { autoClose: 3000 });
+  }
+
+
+
+  return (
+    isLoading ? <Loading /> :
+      <div className="cover">
+        <div className="contact">
+          <div className="close">
+          <ToastContainer />
+            <ImCross
+              onClick={() => {
+                setShowModal(false);
+                getModalState(showModal);
+              }}
+              style={{
+                opacity: "0.9",
+                with: "25px",
+                padding: "5px",
+                color: "black",
+                display: "flex",
+                justifyItems: "end",
+                height: "30px",
+                width: "30px",
+                alignSelf: "end",
+              }}
             />
-            <input
-              onChange={handleChange}
-              type="email"
-              name="email"
-              id="email"
-              placeholder="Email"
-              required="required"
-            />
-            <input
-              onChange={handleChange}
-              type="tel"
-              name="tel"
-              id="tel"
-              placeholder="Phone Number"
-              required="required"
-            />
-            <input
-              type="text"
-              name="subject"
-              id="subject"
-              placeholder="Subject"
-              required="required"
-              onChange={handleChange}
-            />
-            <textarea
-              name="message"
-              id="message"
-              cols="40"
-              rows="3"
-              placeholder="Message"
-              required="required"
-              onChange={handleChange}
-            ></textarea>
-            <input
-              onClick={handleSubmit}
-              className="myBtn"
-              type="button"
-              value="Send Inquiry"
-            />
-          </form>
+          </div>
+
+          <div>
+            <h2>Any questions? Send us an inquiry</h2>
+          </div>
+          <div>
+            <div></div>
+            <form action="" method="post">
+              <input
+                onChange={handleChange}
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Full Name"
+                required="required"
+              />
+              <input
+                onChange={handleChange}
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+                required="required"
+              />
+              <input
+                onChange={handleChange}
+                type="tel"
+                name="tel"
+                id="tel"
+                placeholder="Phone Number"
+                required="required"
+              />
+              <input
+                type="text"
+                name="subject"
+                id="subject"
+                placeholder="Subject"
+                required="required"
+                onChange={handleChange}
+              />
+              <textarea
+                name="message"
+                id="message"
+                cols="40"
+                rows="3"
+                placeholder="Message"
+                required="required"
+                onChange={handleChange}
+              ></textarea>
+              <button
+                onClick={handleSubmit}
+                disabled={formData.name ==='' || formData.email ==='' || formData.tel=== '' || formData.subject ==='' || formData.message=== ''}
+                className="myBtn"
+                type="button"
+              > Send Inquiry</button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
